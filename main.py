e15033cd2af29dd1a17b95f0a388f880d98bf883
Mutlyy <mustafa.mutlu@std.yeditepe.edu.tr>
2025-04-21 16:38:41 +0300

    Add files via upload
    SORU_2

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-x=input("Bir sözcük giriniz :")
-print(len(x))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+x=input("lütfen adinizi giriniz :")
+y=input("lütfen soyadinizi giriniz :")
+sonuc = x[0] +"." + y[0]
+print(sonuc)
