ab2ba59699fc2f17d243cc03975a1a720ac9c479
Mutlyy <mustafa.mutlu@std.yeditepe.edu.tr>
2025-04-21 17:11:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 x=input("Bir sözcük giriniz : ")
-z=input("Bir harf giriniz :")
-y=int(input("Bir sayi giriniz :")) -1
-
-cevap=x[:y] + z + x [y+1:]
-print(cevap)
+for harf in x :
+  print(harf + "!")
