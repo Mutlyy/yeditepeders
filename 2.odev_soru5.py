48950d556205314274d55f30d4587ba52a58e85c
Mutlyy <mustafa.mutlu@std.yeditepe.edu.tr>
2025-04-28 17:49:13 +0300

    Add files via upload

---
+++ 2.odev_soru5.py
@@ -0,0 +1,17 @@
+my_list = [122, 123213, 123123211111, -12, -21, 231, 929, -876]
+en_buyuk = my_list[0]
+for sayi in my_list:
+    if sayi > en_buyuk:
+        en_buyuk = sayi
+print("En büyük sayı:", en_buyuk)
+
+
+
+
+
+
+
+
+
+
+
